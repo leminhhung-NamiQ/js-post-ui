418a2cb533ad4c88a0aa45b3feb271d08e425a18
minh hung le <hungminhle@i3-automation.com>
2021-11-18 09:25:01 +0700

    init commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log('hello main.js');
+
+function sayHello() {
+    console.log('hello main.js');
+    console.log('hello HungLe');
+
+    if ('hello') {
+        console.log('hello');
+
+        if (!'hello') {
+            console.log('thing');
+        }
+    }
+}
